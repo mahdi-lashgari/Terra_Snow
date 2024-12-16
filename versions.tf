3c7a51fa2c618e707a502ffaf25c138df9abad2c
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 13:51:11 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    snowflake  = {
-      source   = "Snowflake-Labs/snowflake"
-      version  = "1.0.0"
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "1.0.0"
     }
   }
 }
