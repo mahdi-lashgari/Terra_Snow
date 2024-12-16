158ce52911916964c93dc9e89ed270b798bf6e27
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 13:44:53 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,17 +1,11 @@
 terraform {
   required_version = "~> 1.9.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-
     snowflake = {
       source                = "Snowflake-Labs/snowflake"
       version               = "~> 0.83"
       configuration_aliases = [snowflake.accountadmin]
     }
-
   }
 }
 
