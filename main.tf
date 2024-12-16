11358db62ee7992eb36e9f759dcd41d460402a37
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 21:54:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
-resource "snowflake_database" "demo_db" {
+resource "snowflake_database" "databases" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
 
-resource "snowflake_schema" "demo_schema" {
+resource "snowflake_schema" "schemas" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 
+  depends_on = [snowflake_database.databases]
+
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
