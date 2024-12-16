f4320faed8ad8851f4307169a9b4066e621b3ac6
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 16:36:44 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,12 @@
 variable "snowflake_account" {
   type  = string
-  value = var.snowflake_account
 }
 
 variable "snowflake_username" {
   type  = string
-  value = var.snowflake_username
 }
 
 variable "snowflake_password" {
   type      = string
   sensitive = true
-  value     = var.snowflake_password
 }
