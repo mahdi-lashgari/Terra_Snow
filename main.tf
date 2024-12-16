d64c194b27d1b08c52d31a5dba07dae31caadf6c
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 18:10:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+#
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
