773765e506e08d94770f8d5bb22695782d1a1a1c
Mehdi Lashgari <mehdi.lashgari@MacBookPro.fritz.box>
2024-12-16 18:07:37 +0100

    commit

--- versions.tf
+++ versions.tf
@@ -2,17 +2,15 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "1.0.0"
+      version = "~> 0.87"
     }
   }
 }
-
 #
 provider "snowflake" {
   alias         = "accountadmin"
   user          = var.snowflake_username
   account       = var.snowflake_account
   password      = var.snowflake_password
-  authenticator = "JWT"
   role          = "ACCOUNTADMIN"
 }
