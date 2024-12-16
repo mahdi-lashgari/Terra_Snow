805efead434d1162027531c963cfc87338a8b860
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 13:04:31 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -18,7 +18,7 @@ provider "snowflake" {
   alias         = "accountadmin"
   user          = var.snowflake_username
   account       = var.snowflake_account
-  private_key   = var.snowflake_password
+  password      = var.snowflake_password
   authenticator = "JWT"
   role          = "ACCOUNTADMIN"
 }
