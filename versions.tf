75ddeab3b8da616315a82924a97309a56a9dd510
mahdi-lashgari <mahdi_lashgari@yahoo.com>
2024-12-16 16:12:18 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -10,9 +10,9 @@ terraform {
 #
 provider "snowflake" {
   alias         = "accountadmin"
-  user          = snowflake_username
-  account       = snowflake_account
-  password      = snowflake_password
+  user          = var.snowflake_username
+  account       = var.snowflake_account
+  password      = var.snowflake_password
   authenticator = "JWT"
   role          = "ACCOUNTADMIN"
 }
